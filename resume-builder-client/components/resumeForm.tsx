a25d0e727b6462bc575896de107f93e5703503f6
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-18 13:22:03 -0700

    created labels

--- resume-builder-client/components/resumeForm.tsx
+++ resume-builder-client/components/resumeForm.tsx
@@ -2,13 +2,52 @@ import React from "react";
 
 function ResumeForm() {
   return (
-    <div>
-      <div className="labelName">Label Name</div>
-      <textarea
-        placeholder="Give a man a mask, and he will tweet the truth.."
-        // value={}
-        // onChange={}
-      ></textarea>
+    <div className="formContainer">
+      <div className="inputContainer">
+        <div className="labelName">First Name</div>
+        <textarea
+          placeholder="e.g. John"
+          // value={}
+          // onChange={}
+        ></textarea>
+      </div>
+
+      <div className="inputContainer">
+        <div className="labelName">Last Name</div>
+        <textarea
+          placeholder="e.g. Smith"
+          // value={}
+          // onChange={}
+        ></textarea>
+      </div>
+
+      <style jsx>{`
+        textarea {
+          width: 250px;
+          height: 30px;
+          resize: none;
+          line-height: 30px;
+          padding-left: 10px;
+          font-size: 14px;
+        }
+        .formContainer {
+          width: 400px;
+          height: 600px;
+          border: 1px solid grey;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-family: arial;
+          flex-direction: column;
+        }
+        .labelName {
+          font-size: 14px;
+          margin-bottom: 5px;
+        }
+        .inputContainer {
+          margin-bottom: 10px;
+        }
+      `}</style>
     </div>
   );
 }
